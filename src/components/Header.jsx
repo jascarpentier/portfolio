58f78b4bbd493a339ce59a3e3fe14db1d295c3b5
jasmine carpentier <jascarpentier@gmail.com>
2019-09-06 20:06:22 -0400

    overlay project page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,35 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div className='header'>
-        <h1>About me</h1>
-        <p>I am an adaptable Front-End Developer and Marketing professional who loves using technology to create new communities and help companies and brands tell their story. My experience in both marketing and development gives me the unique ability to craft relatable narratives that are accompanied by stunning visual presentations.</p>
+      <div className='frontPage'>
+        <div className='header'>
+          <h1>About me</h1>
+          <p>I am an adaptable Front-End Developer and Marketing professional who loves using technology to create new communities and help companies and brands tell their story. My experience in both marketing and development gives me the unique ability to craft relatable narratives that are accompanied by stunning visual presentations.</p>
+        </div>
+
+        <div className='skills'>
+          <h3>Skills & Tools</h3>
+          <img className='iconSkills' src='https://i.imgur.com/xwLfHEN.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/D1G8jr7.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/z0kUmXJ.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/URWnWo4.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/ciruipm.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/Iyq0G00.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/7xaAKtV.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/2eXljUN.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/TE0JfO8.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/RjV1bOm.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/QwpYPQk.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/IEYpEQf.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/sviC6NO.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/wHpDY1p.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/1t4j8lr.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/AqboT1d.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/q4MwwtZ.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/IXpHtqv.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/RjV1bOm.png' alt='github' />
+          <img className='iconSkills' src='https://i.imgur.com/RjV1bOm.png' alt='github' />
+        </div>
       </div>
     )
   }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,29 +5,68 @@ class Projects extends React.Component {
     return (
       <div className='projectStuff'>
         <h1>Portfolio</h1>
-        <div className='projectThings'>
+
+
+        <div className='projectH1'>
           <h5>Harbour</h5>
+        </div>
+        <div className='projectThings'>
           <img className='projects' src="https://i.imgur.com/8kbGV5x.png" alt="harbour" />
           <div class="middle">
-            <div class="text">John Doe</div>
+            <div class="text">Multi-user interactive Hostel collective. React-on-rails full stack project.</div>
           </div>
-
+        </div>
+        <div className='porjectLinks'>
           <a className='porfolioText' href='www.harbour.surge.sh'>Visit the site</a>
           <a className='porfolioText' href='https://github.com/jascarpentier/harbour'>View on github</a>
+        </div>
+
+
+        <div className='projectH1'>
           <h5>Cara Mia</h5>
+        </div>
+        <div className='projectThings'>
           <img className='projects' src="https://i.imgur.com/298Dyzp.png" alt="cara mia" />
+          <div class="middle">
+            <div class="text">Remake of a local Italian restaurant in collaboration with the UXDI students</div>
+          </div>
+        </div>
+        <div className='porjectLinks'>
           <a className='porfolioText' href='www.cara-mia-redesign.surge.sh'>Visit the site</a>
           <a className='porfolioText' href='https://github.com/mishakessler/cara-mia-redesign'>View on github</a>
+        </div>
+
+
+        <div className='projectH1'>
           <h5>Space Cadet</h5>
+        </div>
+        <div className='projectThings'>
           <img className='projects' src="https://i.imgur.com/EIfmjdJ.png" alt="space cadet" />
+          <div class="middle">
+            <div class="text">An E-Commerce store for books using the Google Books API</div>
+          </div>
+        </div>
+        <div className='porjectLinks'>
           <a className='porfolioText' href='www.spacecadet.surge.sh'>Visit the site</a>
           <a className='porfolioText' href='https://github.com/jascarpentier/spacecadet'>View on github</a>
+        </div>
+
+
+        <div className='projectH1'>
           <h5>Stay Dry</h5>
+        </div>
+        <div className='projectThings'>
           <img className='projects' src="https://i.imgur.com/XXT7tz2.png" alt="stay dry" />
+          <div class="middle">
+            <div class="text">Using the REI climbing API and the Open Weather API to make a climbing app to see the local weather and information on climbing routes</div>
+          </div>
+        </div>
+        <div className='porjectLinks'>
           <a className='porfolioText' href='http://staydry.surge.sh/'>Visit the site</a>
           <a className='porfolioText' href='https://github.com/jascarpentier/magic-seaweed'>View on github</a>
-        </div >
+        </div>
       </div >
+
     )
   }
 }
